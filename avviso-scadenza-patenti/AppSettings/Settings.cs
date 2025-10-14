72524506221f83749416319335576e1086484bd4
Roberto Corradetti <rcorra68@gmail.com>
2025-10-14 16:34:31 +0200

    Removed folder MailOut setting

--- avviso-scadenza-patenti/AppSettings/Settings.cs
+++ avviso-scadenza-patenti/AppSettings/Settings.cs
@@ -3,8 +3,6 @@
     internal class Settings
     {
         public const string SectionName = "Settings";
-
-        public string? MailOutputFolder { get; set; }
         public List<string>? MailBcc { get; set; }
         public MailServer MailServer { get; set; }
     }
